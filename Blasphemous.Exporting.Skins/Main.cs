0eb12331bdb50ddd6715220e558b2c93f5f9b6c7
unknown <brandenek03912@hotmail.com>
2024-06-10 11:53:47 -0400

    init: add base project files

---
+++ Blasphemous.Exporting.Skins/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.Exporting.Skins;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+public class Main : BaseUnityPlugin
+{
+    public static SkinExporter Skins { get; private set; }
+
+    private void Start()
+    {
+        Skins = new SkinExporter();
+    }
+}
---
+++ Blasphemous.Exporting.Skins/SkinExporter.cs
@@ -0,0 +1,13 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.Exporting.Skins;
+
+public class SkinExporter : BlasMod
+{
+    public SkinExporter() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        LogError($"{ModInfo.MOD_NAME} has been initialized");
+    }
+}
